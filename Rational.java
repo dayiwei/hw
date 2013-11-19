7c42fdb820e503f3d2efbc1a91bf240ff7384100
dayiwei <dayiwei@hotmail.com>
2013-11-19 02:48:02 -0500

    Added Phase III and fixed add() and subtract()

--- Rational.java
+++ Rational.java
@@ -28,11 +28,11 @@ public class Rational {
     }
 
     public String toString() {
-	return "" + _numerator + "/" + _denominator;
+	return _numerator + "/" + _denominator;
     }
 
     public double floatValue() {
-	double x = _numeratpr/_denominator;
+	double x = _numerator/_denominator;
 	return x;
     }
 
@@ -44,5 +44,86 @@ public class Rational {
     public void divide(Rational x) {
 	_numerator /= x._denominator;
 	_denominator *= x._numerator;
+    }
+
+    /////////Phase II/////////////
+
+    public void add (Rational x) {
+	_numerator *= x._denominator;
+	_numerator += x._numerator * _denominator;
+	_denominator *= x._denominator;
+
+    }
+
+    public void subtract (Rational x) {
+	_numerator *= x._denominator;
+	_numerator -= x._numerator * _denominator;
+	_denominator *= x._denominator;
+
+    }
+
+    public int gcd() {
+	    
+	int remainder = Math.max(_numerator,_denominator)%Math.min(_numerator,_denominator);
+	int oldremainder = Math.min(_numerator,_denominator);
+	while (remainder != 0) {
+	    oldremainder = remainder;
+	    remainder = Math.min(_numerator,_denominator)%remainder;
+
+	}
+	return oldremainder;
+    }
+
+    public void reduce() {
+	_numerator /= gcd();
+	_denominator /= gcd();
+    }
+
+    //////Phase III///////
+
+    public static int gcd(int num, int den) {
+	int remainder = Math.max(num,den)%Math.min(num,den);
+	int oldremainder = Math.min(num,den);
+	while (remainder != 0) {
+	    oldremainder = remainder;
+	    remainder = Math.min(num,den)%remainder;
+
+	}
+	return oldremainder;
+    }
+
+    public int compareTo(Rational x) {
+	if (_numerator/_denominator == x._numerator/x._denominator)
+	    return 0;
+	else if (_numerator/_denominator >= x._numerator/x._denominator)
+	    return 1;
+	else 
+	    return -1;
+    }
+
+    public static void main(String[] args) {
+
+	Rational a = new Rational();
+	Rational x = new Rational(3 ,5);
+
+	System.out.println("Default Rational: " + a);
+	System.out.println("\nRational(3, 5): " + x);
+
+	Rational r = new Rational(2,3); //Stores the rational number 2/3
+	Rational s = new Rational(1,2); //Stores the rational number 1/2
+	Rational t = new Rational(4,18); //Stores the rational number 4/18
+	System.out.println("r: "+r
+			   + "\ns: "+s
+			   + "\nt: "+t);
+	r.add(s);  //Adds r to s, changes r to 7/6.  s remains 1/2 
+	System.out.println("r + s: " + r);
+	t.reduce(); //Changes t to 2/9
+	System.out.println("t reduced: " + t);
+	
+
+	System.out.println("gcd(10,24): " + gcd(10,24));
+	System.out.println("r.compareTo(s): " + r.compareTo(s));
+	System.out.println("s.compareTo(r): " + s.compareTo(r));
+	System.out.println("t.compareTo(t): " + t.compareTo(t));
     }
 }
